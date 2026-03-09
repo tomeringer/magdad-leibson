ea26d5129b1a8caa9e9f8ec9507e0f74186bd7cd
matarjarden <matar.jarden.46@gmail.com>
2026-03-09 13:27:44 +0200

    black to blue right place

--- runtest.py
+++ runtest.py
@@ -27,7 +27,7 @@ factory = PiGPIOFactory()
 # ---- Stereo / YOLO config ----
 CALIBRATION_FILE_PATH = r"Autonomy/stereo_calibration.pkl"
 LEFT_CAM = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-video-index0"
-RIGHT_CAM = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-video-index0"
+RIGHT_CAM = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-video-index0"
 
 W = 640
 H = 480
