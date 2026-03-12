e924c464c47b71f61df3520e2438722ba6a37fb3
matarjarden <matar.jarden.46@gmail.com>
2026-03-12 16:56:54 +0200

    slower

--- runtest.py
+++ runtest.py
@@ -95,12 +95,12 @@ def drive_reverse(speed: float = 0.45):
     RIGHT_RPWM.value, LEFT_RPWM.value = 0.0, 0.0
 
 
-def turn_right(speed: float = 0.25):
+def turn_right(speed: float = 0.10):
     RIGHT_LPWM.value, LEFT_RPWM.value = speed + 0.04, speed
     RIGHT_RPWM.value, LEFT_LPWM.value = 0.0, 0.0
     
 
-def turn_left(speed: float = 0.25):
+def turn_left(speed: float = 0.10):
     RIGHT_RPWM.value, LEFT_LPWM.value = speed + 0.04, speed
     RIGHT_LPWM.value, LEFT_RPWM.value = 0.0, 0.0
     RIGHT_RPWM.value, LEFT_LPWM.value = speed + 0.01, speed
