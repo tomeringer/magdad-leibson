983cae7425ec44c7614d3d53736269795e8d877f
tomeringer <tomer.ringer.46@gmail.com>
2026-03-15 18:15:21 +0200

    debug the code of the encoders

--- runtest.py
+++ runtest.py
@@ -141,8 +141,8 @@ class YellowJacketEncoder:
 
     def zero(self): self.counts = self._c_last = 0
 
-enc_left = YellowJacketEncoder(pi_enc, 24, 25)
-enc = YellowJacketEncoder(pi_enc, 5, 6)
+enc_left = YellowJacketEncoder(pi_enc, 5, 6)
+enc = YellowJacketEncoder(pi_enc, 24, 25)
 
 class StepperMotor:
     def __init__(self, pins):
