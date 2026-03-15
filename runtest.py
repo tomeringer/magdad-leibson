0cdbeaf2b53e8ddc83dc48ef9afe23fbb2dcebfe
matarjarden <matar.jarden.46@gmail.com>
2026-03-15 20:30:29 +0200

    opsi dupsi

--- runtest.py
+++ runtest.py
@@ -86,20 +86,22 @@ LEFT_LPWM = PWMOutputDevice(21, frequency=1000, initial_value=0, pin_factory=fac
 def stop_drive():
     RIGHT_RPWM.value = RIGHT_LPWM.value = LEFT_RPWM.value = LEFT_LPWM.value = 0.0
 
+speed_diff = 1.08
+
 def drive_forward(speed: float = 0.3):
-    RIGHT_RPWM.value, LEFT_RPWM.value = speed + 0.01, speed
+    RIGHT_RPWM.value, LEFT_RPWM.value = speed * speed_diff, speed
     RIGHT_LPWM.value, LEFT_LPWM.value = 0.0, 0.0
 
 def drive_reverse(speed: float = 0.45):
-    RIGHT_LPWM.value, LEFT_LPWM.value = speed + 0.01, speed
+    RIGHT_LPWM.value, LEFT_LPWM.value = speed * speed_diff, speed
     RIGHT_RPWM.value, LEFT_RPWM.value = 0.0, 0.0
 
 def turn_right(speed: float = 0.1):
-    RIGHT_LPWM.value, LEFT_RPWM.value = speed+ 0.01, speed
+    RIGHT_LPWM.value, LEFT_RPWM.value = speed * speed_diff, speed
     RIGHT_RPWM.value, LEFT_LPWM.value = 0.0, 0.0
     
 def turn_left(speed: float = 0.1):
-    RIGHT_RPWM.value, LEFT_LPWM.value = speed + 0.01, speed
+    RIGHT_RPWM.value, LEFT_LPWM.value = speed * speed_diff, speed
     RIGHT_LPWM.value, LEFT_RPWM.value = 0.0, 0.0
 
 # ============================================================
