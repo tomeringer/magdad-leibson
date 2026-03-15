caae522a05f1d07f1a04ab15d2245f2858c87d3f
matarjarden <matar.jarden.46@gmail.com>
2026-03-15 16:27:53 +0200

    no r cams

--- Autonomy/DBcams.py
+++ Autonomy/DBcams.py
@@ -7,16 +7,21 @@ app = Flask(__name__)
 # Open cameras using V4L2 for Raspberry Pi
 # Indexes are typically 0 and 2 on Linux, change if necessary
 cap_l = cv2.VideoCapture(0, cv2.CAP_V4L2)
-cap_r = cv2.VideoCapture(2, cv2.CAP_V4L2)
+# cap_r = cv2.VideoCapture(2, cv2.CAP_V4L2)
+
+# Verify if cameras are successfully opened
+print(f"[DEBUG] Left camera opened (index 0): {cap_l.isOpened()}")
+print(f"[DEBUG] Right camera opened (index 2): {cap_r.isOpened()}")
+
 
 # Configure MJPG and resolution to save USB bandwidth
 cap_l.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 cap_l.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap_l.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-cap_r.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
-cap_r.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap_r.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# cap_r.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+# cap_r.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# cap_r.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # Generator for video frames
 def generate_frames(camera):
