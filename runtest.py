58be11a7f52da1a4463c13fcf40095ac2c3b36f8
matarjarden <matar.jarden.46@gmail.com>
2026-03-15 20:21:00 +0200

    eq speeds

--- runtest.py
+++ runtest.py
@@ -87,20 +87,18 @@ def stop_drive():
     RIGHT_RPWM.value = RIGHT_LPWM.value = LEFT_RPWM.value = LEFT_LPWM.value = 0.0
 
 def drive_forward(speed: float = 0.3):
-    RIGHT_RPWM.value, LEFT_RPWM.value = speed, speed
+    RIGHT_RPWM.value, LEFT_RPWM.value = speed + 0.01, speed
     RIGHT_LPWM.value, LEFT_LPWM.value = 0.0, 0.0
 
 def drive_reverse(speed: float = 0.45):
-    RIGHT_LPWM.value, LEFT_LPWM.value = speed + 0.04, speed
+    RIGHT_LPWM.value, LEFT_LPWM.value = speed + 0.01, speed
     RIGHT_RPWM.value, LEFT_RPWM.value = 0.0, 0.0
 
 def turn_right(speed: float = 0.1):
-    RIGHT_LPWM.value, LEFT_RPWM.value = speed + 0.04, speed
+    RIGHT_LPWM.value, LEFT_RPWM.value = speed+ 0.01, speed
     RIGHT_RPWM.value, LEFT_LPWM.value = 0.0, 0.0
     
 def turn_left(speed: float = 0.1):
-    RIGHT_RPWM.value, LEFT_LPWM.value = speed + 0.04, speed
-    RIGHT_LPWM.value, LEFT_RPWM.value = 0.0, 0.0
     RIGHT_RPWM.value, LEFT_LPWM.value = speed + 0.01, speed
     RIGHT_LPWM.value, LEFT_RPWM.value = 0.0, 0.0
 
