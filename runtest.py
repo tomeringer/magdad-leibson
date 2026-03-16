bc8ccc124b4e838ff79f86bbcd83a63b50088c24
matarjarden <matar.jarden.46@gmail.com>
2026-03-16 12:51:39 +0200

    0.4

--- runtest.py
+++ runtest.py
@@ -457,7 +457,7 @@ def drive_arc(target_x, target_z):
 
     # 3. Calculate Speed Ratio
     # Faster speed / Slower speed = r_outer / r_inner
-    speed_outer = 0.3
+    speed_outer = 0.4
     speed_inner = speed_outer * (r_inner / r_outer)
 
     # 4. Determine which wheel is which
